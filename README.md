08d3e253587cf9a166984937f9c2c2f0fc1e55f7
Mustafa Yücesan <my96@live.nl>
2020-02-29 10:21:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# DNS-Server
+
+RR stands for resource records. 
+
+4.2 Transport RFC 1035
+
+####Following RR definitions are expected to occur, at leat potentially, in all classes.
+
+
+| RR Name        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| NS      | Name server | an authoritative name server |
+| SOA      | Start of Authority      |   marks the start of a zone of authority |
+| CNAME | canonical name     |    the canonical name for an alias |
+| PTR | pointer |a domain name pointer
+
---
+++ constants.py
@@ -0,0 +1,43 @@
+# Server
+IP = 0.0.0.0
+PORT = 53
+
+# Size limits (in octet)
+
+LABELS = 64
+NAMES = 255
+UDPM = 512
+# TTL = positive values of a signed 32 bit number
+
+# QTYPE values (appear in question part of a query) (superset of TYPE, TYPE's are valid QTYPE's)
+AXFR = 252 # A request for transfer of an entire zone
+MAILB = 253 # A request for mailbox-related records (MV, MG, or MR)
+MAILA = 254 # A request for mail agent RRs (Obsolete - see MX)
+ASTERISK = 255 # A request for all records
+# TYPE values (subset of QTYPE) (resource records)
+A = 1 # a host address
+NS = 2 # an authoritative name server
+MD = 3 # a mail destination (Obsolete - use MX)
+MF = 4 # a mail forwarder (Obsolete - use MX)
+CNAME = 5 # the canonical name for an alias
+SOA = 6 # marks the start of a zone of authority
+MB = 7 # a mailbox domain name (EXPERIMENTAL)
+MG = 8 # a mail group member (EXPERIMENTAL)
+MR = 9 # a null RR (EXPERIMENTAL)
+NULL = 10 # a null RR (EXPERIMENTAL)
+WKS = 11 # a well known service description
+PTR = 12 # a domain name pointer
+HINFO = 13 # host information
+MINFO = 14 # mailbor or mail list information
+MX = 15 # mail exchange
+TXT = 16 # text strings
+
+# QCLASS values (appear in question section of query, superset of CLASS values so every class is a valid QCLASS)
+QCLASSASTERISK = 255 # any class
+
+
+# CLASS values (appear in resource records)
+IN = 1 # the Internet
+CS = 2 # the CSNET class (Obsolete - used only for examples in some obsolete RFCs)
+CH = 3 # the chaos class
+HS = 4 # Hesiod [Dyer 87]
